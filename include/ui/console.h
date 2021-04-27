5010c2a96393c2960bf56db005b22b87774147d6
xXTeraXx <57876649+xXTeraXx@users.noreply.github.com>
2021-04-27 13:48:58 -0400

    Update console.h
    this is the only change im making

--- include/ui/console.h
+++ include/ui/console.h
@@ -26,7 +26,7 @@
 #define QEMU_CAPS_LOCK_LED   (1 << 2)
 
 /* in ms */
-#define GUI_REFRESH_INTERVAL_DEFAULT    30
+#define GUI_REFRESH_INTERVAL_DEFAULT    16
 #define GUI_REFRESH_INTERVAL_IDLE     3000
 
 /* Color number is match to standard vga palette */
